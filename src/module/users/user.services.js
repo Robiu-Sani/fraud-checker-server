4ab43958a0b011f57dbe0710c72d9f862e509a71
Robiu-Sani <hafazrobiussani@gmail.com>
2025-02-03 14:14:32 +0600

    adding jwt

--- src/module/users/user.services.js
+++ src/module/users/user.services.js
@@ -1,5 +1,9 @@
 import Users from './user.model.js';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const createUsersIntoDB = async (payload) => {
   const result = await Users.create(payload);
@@ -32,35 +36,37 @@ const deleteSingleUserByIdIntoDB = async (id) => {
 };
 
 const loginServices = async (payload) => {
-  try {
-    let user;
-    if (payload.email) {
-      user = await Users.findOne({ email: payload.email });
-    } else if (payload.number) {
-      user = await Users.findOne({ number: payload.number });
-    }
-
-    if (!user) {
-      return {
-        status: false,
-        message: 'No user found with this email or number.',
-      };
-    }
-
-    const isPasswordValid = await bcrypt.compare(
-      payload.password,
-      user.password,
-    );
-    if (!isPasswordValid) {
-      return { status: false, message: 'Incorrect password.' };
-    }
-
-    const userInfo = { ...user.toObject(), password: undefined };
-    return { status: true, message: 'Login successful', data: userInfo };
-  } catch (error) {
-    console.error('Error during login:', error);
-    return { status: false, message: 'Something went wrong.' };
+  let user;
+  if (payload.email) {
+    user = await Users.findOne({ email: payload.email });
+  } else if (payload.number) {
+    user = await Users.findOne({ number: payload.number });
   }
+
+  if (!user) {
+    return {
+      status: false,
+      message: 'No user found with this email or number.',
+    };
+  }
+
+  const isPasswordValid = await bcrypt.compare(payload.password, user.password);
+  if (!isPasswordValid) {
+    return { status: false, message: 'Incorrect password.' };
+  }
+
+  const jwtPayload = {
+    id: user._id,
+    role: user.type,
+    email: user.email,
+  };
+
+  const eccessToken = jwt.sign(jwtPayload, process.env.SECRET_TOKEN, {
+    expiresIn: 60 * 60,
+  });
+
+  const userInfo = { ...user.toObject(), password: undefined };
+  return { status: true, message: 'Login successful', data: userInfo };
 };
 
 const userServices = {
