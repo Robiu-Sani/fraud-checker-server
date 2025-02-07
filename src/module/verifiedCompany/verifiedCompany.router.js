7cac1f7180b9b5c9ad916db1dc9ac33d2695b5dc
Robiu-Sani <hafazrobiussani@gmail.com>
2025-02-07 10:41:04 +0600

    verifued company is done

--- src/module/verifiedCompany/verifiedCompany.router.js
+++ src/module/verifiedCompany/verifiedCompany.router.js
@@ -2,10 +2,10 @@ import express from 'express';
 import VerifyControllar from './verifiedCompany.controllar.js';
 const router = express.Router();
 
-router.post('/', VerifyControllar.createCompanyVerification);
+router.post('/create', VerifyControllar.createCompanyVerification);
 router.get('/', VerifyControllar.getAllCompanyVerifications);
-router.get('/:id', VerifyControllar.getSingleCompanyVerification);
-router.put('/:id', VerifyControllar.updateCompanyVerification);
-router.delete('/:id', VerifyControllar.deleteCompanyVerification);
+router.get('/single/:id', VerifyControllar.getSingleCompanyVerification);
+router.put('/single/:id', VerifyControllar.updateCompanyVerification);
+router.delete('/single/:id', VerifyControllar.deleteCompanyVerification);
 
 export const verify_route = router;
